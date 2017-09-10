1a53ce61f7f5b1ebb5ddd6b229caaf533a1b89aa
Leo Gordon <ens-lg4@users.noreply.github.com>
2017-09-10 20:54:40 +0100

    solved the math of start/end of the elliptic arcs using ellips equation in polar coordinates (3 small ellipses only)

--- ellipse.py
+++ ellipse.py
@@ -18,18 +18,40 @@ def midpoint(F1, F2):
     "Find the midpoint of two 2D points"
     return ((F1[0]+F2[0])/2, (F1[1]+F2[1])/2)
 
-def draw_ellipse(dwg, F1, F2, d, colour='grey'):
+def scalar_product(P0, P1, P2):
+    "Find the scalar product of two 2D vectors given by their endpoints"
+    return (P1[0]-P0[0])*(P2[0]-P0[0])+(P1[1]-P0[1])*(P2[1]-P0[1])
+
+def draw_ellipse(dwg, F1, F2, Pl, d, colour='grey', marks=False):
     "Draw a tilted ellipse given two foci and the length of slack part of the rope attached to them"
     C               = midpoint(F1, F2)
     cf              = distance(F1, F2)/2
     tilt_deg        = math.degrees( math.atan2(F2[1]-F1[1], F2[0]-F1[0]) )
     a               = d/2
-    b               = math.sqrt( a*a - cf*cf )
+    b               = math.sqrt( a**2 - cf**2 )
         # Here we rotate the coordinates so that inside the SVG group element the major axis of the ellipse is horizontal
     target_group    = dwg.g( stroke=colour, stroke_width='2', fill='none', transform='rotate(%f,%f,%f)' % (tilt_deg, C[0], C[1]) )
     target_group.add( dwg.ellipse( center=C, r=(a,b) ) )
     target_group.add( dwg.circle( center=(C[0]-cf,C[1]), r=5, stroke=F1[2] ) )
     target_group.add( dwg.circle( center=(C[0]+cf,C[1]), r=5, stroke=F2[2] ) )
+    if marks:
+        cos_alpha       = scalar_product(F1, F2, Pl)/(distance(F1,F2)*distance(F1, Pl))
+        cos_phi         = -cos_alpha
+        sin_phi         = math.sqrt(1-cos_phi**2)
+        rho             = b**2/(a-cf*cos_phi)
+        xl              = rho*cos_phi
+        yl              = -rho*sin_phi
+        target_group.add( dwg.circle( center=(C[0]-cf+xl,C[1]+yl), r=10, stroke=F1[2], fill=Pl[2] ) )
+
+        cos_beta        = scalar_product(F2, F1, Pl)/(distance(F1,F2)*distance(F2, Pl))
+        beta            = math.degrees( math.acos(cos_beta) )
+        cos_phi         = cos_beta
+        sin_phi         = math.sqrt(1-cos_phi**2)
+        rho             = b**2/(a+cf*cos_phi)
+        xl              = rho*cos_phi
+        yl              = -rho*sin_phi
+        target_group.add( dwg.circle( center=(C[0]+cf+xl,C[1]+yl), r=15, stroke=F2[2], fill=Pl[2] ) )
+
     dwg.add( target_group )
 
 def draw_ellipsystem(P1, P2, P3, slack=200, filename="hexaellipse.svg", canvas_size=(1000,1000)):
@@ -40,12 +62,12 @@ def draw_ellipsystem(P1, P2, P3, slack=200, filename="hexaellipse.svg", canvas_s
     tight_loop      = d12 + d23 + d31
     loop_length     = tight_loop+slack
     dwg             = svgwrite.Drawing(filename=filename, debug=True, size=canvas_size)
-    draw_ellipse(dwg, P1, P2, loop_length-d12,      colour=P3[2])
-    draw_ellipse(dwg, P1, P2, loop_length-d23-d31,  colour=P3[2])
-    draw_ellipse(dwg, P2, P3, loop_length-d23,      colour=P1[2])
-    draw_ellipse(dwg, P2, P3, loop_length-d12-d31,  colour=P1[2])
-    draw_ellipse(dwg, P3, P1, loop_length-d31,      colour=P2[2])
-    draw_ellipse(dwg, P3, P1, loop_length-d12-d23,  colour=P2[2])
+    draw_ellipse(dwg, P1, P2, P3, loop_length-d12,      colour=P3[2])
+    draw_ellipse(dwg, P1, P2, P3, loop_length-d23-d31,  colour=P3[2], marks=True)
+    draw_ellipse(dwg, P3, P2, P1, loop_length-d23,      colour=P1[2])
+    draw_ellipse(dwg, P2, P3, P1, loop_length-d12-d31,  colour=P1[2], marks=True)
+    draw_ellipse(dwg, P1, P3, P2, loop_length-d31,      colour=P2[2])
+    draw_ellipse(dwg, P3, P1, P2, loop_length-d12-d23,  colour=P2[2], marks=True)
     dwg.save()
 
 if __name__ == '__main__':
