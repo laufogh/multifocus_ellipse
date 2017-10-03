e813c1fc2844d3c3257cf6793b174a1e3aff210b
Leo Gordon <ens-lg4@users.noreply.github.com>
2017-10-03 16:44:21 +0100

    moved ellipse-specific parameters into a separate class

--- ellipse.py
+++ ellipse.py
@@ -46,67 +46,60 @@ def turn_and_scale(Z, D, cos_f, rho):
 
     return (A_x, A_y)
 
+class Ellipse:
+    "Computes and stores parameters of the ellipse and provides some helper geometry methods"
+
+    def __init__(self, F1, F2, d):
+        self.F1 = F1
+        self.F2 = F2
+        self.c  = distance(F1, F2)/2
+        self.a  = d/2
+        self.b  = math.sqrt( self.a**2 - self.c**2 )
+
+    def point_on_the_ellipse(self, cos_f, focus_sign=-1):
+        rho     = self.b**2 / (self.a + focus_sign * self.c * cos_f)
+        (Z, D)  = (self.F1, self.F2) if focus_sign==-1 else (self.F2, self.F1)
+        return turn_and_scale(Z, D, cos_f, -focus_sign*rho)
+
+    def tilt_deg(self):
+        return math.degrees( math.atan2(self.F2[1]-self.F1[1], self.F2[0]-self.F1[0]) )
+
 
 def draw_ellipsystem(P, slack=250, show_leftovers=False, show_tickmarks=True, filename="example.svg", canvas_size=(1000,1000), pencil_mark_fraction=None):
     "Draw a simplified system of 2*len(P) ellipse fragments that make up the sought-for smooth convex shape"
 
     dwg             = svgwrite.Drawing(filename=filename, debug=True, size=canvas_size)
 
-    def draw_ellipse_fragment(Pprev, F1, F2, Pnext, d):
-        "Draw an ellipse fragment given two foci, the third point used for cut-off and the length of slack part of the rope attached to the foci"
+    def draw_ellipse_fragment( ellipse, A, B, tick_mark_colour=None ):
+        "Draw an ellipse fragment given the ellipse and two limits"
 
-            # internal absolute measurements of the ellipse (also available to the nested function) :
-        c               = distance(F1, F2)/2
-        a               = d/2
-        b               = math.sqrt( a**2 - c**2 )
-
-        def point_on_the_ellipse(cos_f, focus_sign=-1):
-            rho     = b**2 / (a + focus_sign * c * cos_f)
-            (Z, D)  = (F1, F2) if focus_sign==-1 else (F2, F1)
-            return turn_and_scale(Z, D, cos_f, -focus_sign*rho)
-
-        tilt_deg        = math.degrees( math.atan2(F2[1]-F1[1], F2[0]-F1[0]) )
+        tilt_deg        = ellipse.tilt_deg()
         target_group    = dwg.g( fill='none' )
 
-            # A and B are start and end points of the ellipse fragment:
-        if Pprev:
-            cos_for_A       = -three_point_cosine(F2, F1, Pprev)
-            cos_for_B       =  three_point_cosine(F1, F2, Pnext)
-            clockwise_sign  =  1
-        else:
-            cos_for_A       = -three_point_cosine(F1, F2, Pnext)
-            cos_for_B       =  three_point_cosine(F2, F1, Pnext)
-            clockwise_sign  = -1
-
-        (Ax,Ay)     = point_on_the_ellipse( cos_for_A, focus_sign=-clockwise_sign )
-        (Bx,By)     = point_on_the_ellipse( cos_for_B, focus_sign= clockwise_sign )
-
             # visible part of the component ellipse:
-        target_group.add( dwg.path( d="M %f,%f A %f,%f %f 0,1 %f,%f" % (Ax, Ay, a, b, tilt_deg, Bx, By), stroke=F1[2], stroke_width=6, stroke_dashoffset=10, stroke_dasharray='10,10') )
+        target_group.add( dwg.path( d="M %f,%f A %f,%f %f 0,1 %f,%f" % (A[0], A[1], ellipse.a, ellipse.b, tilt_deg, B[0], B[1]), stroke=ellipse.F1[2], stroke_width=6, stroke_dashoffset=10, stroke_dasharray='10,10') )
 
-        target_group.add( dwg.path( d="M %f,%f A %f,%f %f 0,1 %f,%f" % (Ax, Ay, a, b, tilt_deg, Bx, By), stroke=F2[2], stroke_width=6, stroke_dasharray='10,10' ) )
+        target_group.add( dwg.path( d="M %f,%f A %f,%f %f 0,1 %f,%f" % (A[0], A[1], ellipse.a, ellipse.b, tilt_deg, B[0], B[1]), stroke=ellipse.F2[2], stroke_width=6, stroke_dasharray='10,10' ) )
 
             # invisible part of the component ellipse:
         if show_leftovers:
-            target_group.add( dwg.path( d="M %f,%f A %f,%f %f 1,0 %f,%f" % (Ax, Ay, a, b, tilt_deg, Bx, By), stroke=F1[2], stroke_width=2, stroke_dasharray='5,15' ) )
-            target_group.add( dwg.path( d="M %f,%f A %f,%f %f 1,0 %f,%f" % (Ax, Ay, a, b, tilt_deg, Bx, By), stroke=F2[2], stroke_width=2, stroke_dasharray='5,15', stroke_dashoffset=10 ) )
+            target_group.add( dwg.path( d="M %f,%f A %f,%f %f 1,0 %f,%f" % (A[0], A[1], ellipse.a, ellipse.b, tilt_deg, B[0], B[1]), stroke=ellipse.F1[2], stroke_width=2, stroke_dasharray='5,15' ) )
+            target_group.add( dwg.path( d="M %f,%f A %f,%f %f 1,0 %f,%f" % (A[0], A[1], ellipse.a, ellipse.b, tilt_deg, B[0], B[1]), stroke=ellipse.F2[2], stroke_width=2, stroke_dasharray='5,15', stroke_dashoffset=10 ) )
 
         if show_tickmarks:
-            tick_mark_colour    = F1[2] if Pprev else F2[2]
-#            target_group.add( dwg.circle( center=(Ax,Ay), r=12, stroke=F1[2], fill=Pnext[2] ) )    # "from" tick mark
-            target_group.add( dwg.circle( center=(Bx,By), r=8, stroke=tick_mark_colour, fill=tick_mark_colour ) )     # "to" tick mark
+            target_group.add( dwg.circle( center=(B[0],B[1]), r=8, stroke=tick_mark_colour, fill=tick_mark_colour ) )     # "to" tick mark
 
         if pencil_mark_fraction is not None:
-                # find the angles relative to F1 in local coordinates:
-            gamma   = math.acos( three_point_cosine(F2, F1, (Ax,Ay)) )
-            delta   = math.acos( three_point_cosine(F2, F1, (Bx,By)) )
+                # find the angles relative to ellipse.F1 in local coordinates:
+            gamma   = math.acos( three_point_cosine(ellipse.F2, ellipse.F1, (A[0],A[1])) )
+            delta   = math.acos( three_point_cosine(ellipse.F2, ellipse.F1, (B[0],B[1])) )
                 # now we can create any convex combination and map it onto the corresponding ellipse fragment:
             mix     = gamma * (1-pencil_mark_fraction) + delta * pencil_mark_fraction
             (Mx,My) = point_on_the_ellipse( math.cos( mix ) )
 
             target_group.add( dwg.circle( center=(Mx,My), r=5,     stroke='blue', stroke_width=1, fill='none' ) )    # "mix" tick mark
-            target_group.add( dwg.line( start=F1[0:2], end=(Mx,My), stroke='blue', stroke_width=1  ) )
-            target_group.add( dwg.line( start=F2[0:2], end=(Mx,My), stroke='blue', stroke_width=1  ) )
+            target_group.add( dwg.line( start=ellipse.F1[0:2], end=(Mx,My), stroke='blue', stroke_width=1  ) )
+            target_group.add( dwg.line( start=ellipse.F2[0:2], end=(Mx,My), stroke='blue', stroke_width=1  ) )
 
         dwg.add( target_group )
 
@@ -117,8 +110,20 @@ def draw_ellipsystem(P, slack=250, show_leftovers=False, show_tickmarks=True, fi
         dist_2_prev.append( distance(P[i], P[i-1]) )
 
     for i in range(n):
-        draw_ellipse_fragment(P[i-1],   P[i],   P[i+1-n],   P[i+2-n],   slack+dist_2_prev[i+1-n] )                      # consecutive foci
-        draw_ellipse_fragment(None,     P[i],   P[i+2-n],   P[i+1-n],   slack+dist_2_prev[i+1-n]+dist_2_prev[i+2-n] )   # skip-one focus
+            # consecutive foci first:
+        ellipse         = Ellipse(P[i], P[i+1-n], slack+dist_2_prev[i+1-n])
+        cos_for_A       = -three_point_cosine(ellipse.F2, ellipse.F1, P[i-1])   # Pprev
+        A               = ellipse.point_on_the_ellipse( cos_for_A, focus_sign=-1 )
+        cos_for_B       =  three_point_cosine(ellipse.F1, ellipse.F2, P[i+2-n]) # Pnext
+        B               = ellipse.point_on_the_ellipse( cos_for_B, focus_sign=1 )
+        draw_ellipse_fragment( ellipse, A, B, tick_mark_colour=P[i][2])
+
+            # now skip one focus:
+        ellipse         = Ellipse(P[i], P[i+2-n], slack+dist_2_prev[i+1-n]+dist_2_prev[i+2-n])
+        A               = B
+        cos_for_B       =  three_point_cosine(ellipse.F2, ellipse.F1, P[i+1-n]) # Pnext
+        B               = ellipse.point_on_the_ellipse( cos_for_B, focus_sign=-1 )
+        draw_ellipse_fragment( ellipse, A, B, tick_mark_colour=P[i+2-n][2])
 
     dwg.save()
 
