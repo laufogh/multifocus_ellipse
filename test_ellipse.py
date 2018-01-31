6d2368e1b1de928d36f1055e29bfc5de3acab3c0
Leo Gordon <ens-lg4@users.noreply.github.com>
2018-01-31 18:11:04 +0000

    report individual non-matches

--- test_ellipse.py
+++ test_ellipse.py
@@ -18,7 +18,7 @@ class EllipseTests(unittest.TestCase):
     def test_recreate_and_compare_examples(self):
         ellipse.create_drawings( self.test_dir )
         for filename in ['three_foci_without_leftovers.svg', 'three_foci_with_leftovers.svg', 'four_foci_without_leftovers.svg', 'four_foci_with_leftovers.svg', 'seven_foci_different_slacks.svg']:
-            self.assertTrue(filecmp.cmp( 'examples/'+filename, self.test_dir+'/'+filename))
+            self.assertTrue(filecmp.cmp( 'examples/'+filename, self.test_dir+'/'+filename), msg=filename+" is ok")
 
 if __name__ == '__main__':
     unittest.main()
